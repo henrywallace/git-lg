bcc3be631fc6c97731b776be691f60d1565f03c9
Henry Wallace <henry@henrywallace.io>
2019-02-06 05:52:28 +0000

    write to stdout as array, log, and update README

--- README.md
+++ README.md
@@ -1,8 +1,50 @@
 git-lg
 ------
 
-`git lg` is like `git log` but with structured output.
+`git-lg` is like `git log` but with structured output.
 
 [![Linux build status](https://travis-ci.org/henrywallace/git-lg.svg)](https://travis-ci.org/henrywallace/git-lg.svg)
+
+If the binary is in your path you can run simply run `git lg` as git will
+automatically pick it up as a subcommand. Use the `GIT_DIR=/path/to/.git` env
+to control which repo to use.
+
+While one could use git log [pretty
+formatting](https://git-scm.com/docs/pretty-formats) like `git log
+--pretty=format='%an,aI,%s'`, parsing the output can be cumbersome, possibly
+having to deal with illegal sequences. This tool aims to be a thin layer to
+allow easy parsing with other tools like `jq`: https://stedolan.github.io/jq/.
+
+Currently the only output format is an array of commit objects, with only the
+following fields:
+```json
+[
+  {
+    "author_name": "kennytm",
+    "author_date": "2019-02-05T15:29:17",
+    "committer_name": "GitHub",
+    "committer_date": "2019-02-05T15:29:17",
+    "subject": "Rollup merge of #58169 - boringcactus:patch-1, r=alexcrichton",
+    "body": "\nUpdate contributor name in .mailmap\n\nfollowing up on email correspondence with @steveklabnik\n"
+  }
+]
+```
+
+### Example:
+```
+$ git clone https://github.com/rust-lang/rust /tmp/rust
+$ export GIT_DIR=/tmp/rust/.git
+$ git lg | jq -r '.[].author_name' | sort | uniq -c | sort -nrk1 | head
+  14399 bors
+   5503 Brian Anderson
+   4791 Alex Crichton
+   3834 Niko Matsakis
+   2828 Patrick Walton
+   2228 Graydon Hoare
+   1980 Manish Goregaokar
+   1757 Guillaume Gomez
+   1689 kennytm
+   1671 Steve Klabnik
+```
 
 Dual-licensed under MIT or the [UNLICENSE](http://unlicense.org).
--- src/main.rs
+++ src/main.rs
@@ -6,35 +6,75 @@ extern crate serde;
 extern crate serde_json;
 #[macro_use]
 extern crate serde_derive;
+#[macro_use]
+extern crate log;
 
 use chrono::NaiveDateTime;
+use serde::ser::{SerializeSeq, Serializer};
 
+// TODO: Let's add some config!
+// Ideas:
+// - dir of git repo, with or without .git specified
+// - commits since some date, or relative duration
+// - whether to output json-lines or json array
+// - whether to output to a file
 struct Config {}
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Commit {
-    subject: String,
+struct StructuredCommit {
     author_name: String,
     author_date: NaiveDateTime,
+    committer_name: String,
     committer_date: NaiveDateTime,
+    subject: String,
+    body: String,
 }
 
 fn run(_config: &Config) -> Result<(), failure::Error> {
     let repo = git2::Repository::open_from_env()?;
+
+    let out = std::io::stdout();
+    let mut ser = serde_json::Serializer::new(out);
+    let mut seq = ser.serialize_seq(None)?;
+
     let mut revwalk = repo.revwalk()?;
     revwalk.push_head()?;
-    let mut commits: Vec<Commit> = vec![];
     for oid in revwalk {
         let commit = repo.find_commit(oid?)?;
-        commits.push(Commit {
-            subject: commit.summary().unwrap().to_owned(),
-            author_name: commit.author().name().unwrap().to_owned(),
-            author_date: NaiveDateTime::from_timestamp(commit.author().when().seconds(), 0),
-            committer_date: NaiveDateTime::from_timestamp(commit.committer().when().seconds(), 0),
-        });
-        let x = serde_json::to_string(commits.last().unwrap()).unwrap();
-        println!("{}", x);
+        let (author, committer) = (commit.author(), commit.committer());
+
+        // TODO: Let's learn how to set the subject, body tuple in one go. Feels kinda clunky right
+        // now. And why do we need <Vec<_>> on collect? And what's with all the to_owned() stuff?
+        let pair = commit
+            .message()
+            .unwrap()
+            .splitn(2, '\n')
+            .collect::<Vec<_>>();
+        let subject = pair[0];
+        let body = if pair.len() > 1 { pair[1] } else { "" };
+        let sc = StructuredCommit {
+            author_name: author.name().unwrap().to_owned(),
+            author_date: NaiveDateTime::from_timestamp(author.when().seconds(), 0),
+            committer_name: committer.name().unwrap().to_owned(),
+            committer_date: NaiveDateTime::from_timestamp(committer.when().seconds(), 0),
+            subject: subject.to_owned(),
+            body: body.to_owned(),
+        };
+        seq.serialize_element(&sc)?;
+        // TODO: Why is this so slow? git log --stat is pretty snippy. This takes like 500ms per
+        // commit.
+        //
+        // let a = if commit.parents().len() == 1 {
+        //     let parent = commit.parent(0)?;
+        //     Some(parent.tree()?)
+        // } else {
+        //     None
+        // };
+        // let b = commit.tree()?;
+        // let diff = repo.diff_tree_to_tree(a.as_ref(), Some(&b), None)?;
+        // println!("{}", diff.stats()?.insertions());
     }
+    seq.end()?;
 
     Ok(())
 }
@@ -44,8 +84,8 @@ fn main() {
         .about("git log with structured output")
         .get_matches();
     if let Err(e) = run(&Config {}) {
-        println!("HELLO");
-        println!("{:?}", e);
+        // TODO: Is this the way one logs errors? Are there better idioms?
+        error!("{}", e);
         ::std::process::exit(1);
     }
 }
